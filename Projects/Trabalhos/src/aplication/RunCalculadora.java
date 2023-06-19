406f635dbe214fe73090a05dfa86966607d264d1
Naoki Miura <119423433+imaterialtree@users.noreply.github.com>
2023-06-19 18:47:04 -0300

    calculadora utilitario

---
+++ Projects/Trabalhos/src/aplication/RunCalculadora.java
@@ -0,0 +1,42 @@
+package aplication;
+
+import t1.Calculadora;
+
+import javax.swing.*;
+import java.util.Arrays;
+
+public class RunCalculadora {
+    public static void main(String[] args) throws Exception {
+        int qtdNum = (int) JOptionPane.showInputDialog(
+                null, "Quantos números?",
+                "Calculadora", JOptionPane.PLAIN_MESSAGE,
+                null, new Object[] {2, 3, 4}, 2
+        );
+        double[] nums = new double[qtdNum];
+        for (int i = 0; i < qtdNum; i++) {
+            nums[i] = Double.parseDouble(JOptionPane.showInputDialog("Digite um número"));
+        }
+        String resultado = "Números = "+ Arrays.toString(nums);
+        switch (qtdNum) {
+            case 2 -> {
+                resultado += "\nSoma = "+Calculadora.soma(nums[0], nums[1]);
+                resultado += "\nSubtração = "+Calculadora.subtracao(nums[0], nums[1]);
+                resultado += "\nMultiplicação = "+Calculadora.multiplicar(nums[0], nums[1]);
+                resultado += "\nDividisão = "+Calculadora.dividir(nums[0], nums[1]);
+            }
+            case 3 -> {
+                resultado += "\nSoma = "+Calculadora.soma(nums[0], nums[1], nums[2]);
+                resultado += "\nSubtração = "+Calculadora.subtracao(nums[0], nums[1], nums[2]);
+                resultado += "\nMultiplicação = "+Calculadora.multiplicar(nums[0], nums[1], nums[2]);
+                resultado += "\nDividisão = "+Calculadora.dividir(nums[0], nums[1], nums[2]);
+            }
+            case 4 -> {
+                resultado += "\nSoma = "+Calculadora.soma(nums[0], nums[1], nums[2], nums[3]);
+                resultado += "\nSubtração = "+Calculadora.subtracao(nums[0], nums[1], nums[2], nums[3]);
+                resultado += "\nMultiplicação = "+Calculadora.multiplicar(nums[0], nums[1], nums[2], nums[3]);
+                resultado += "\nDividisão = "+Calculadora.dividir(nums[0], nums[1], nums[2], nums[3]);
+            }
+        }
+        JOptionPane.showMessageDialog(null, resultado, "Resultados", JOptionPane.PLAIN_MESSAGE);
+    }
+}
---
+++ Projects/Trabalhos/src/t1/Calculadora.java
@@ -0,0 +1,51 @@
+package t1;
+
+public class Calculadora {
+    
+    // soma
+    public static double soma(double n1, double n2) {
+        return n1 + n2;
+    }
+    public static double soma(double n1, double n2, double n3) {
+        return n1 + n2 + n3;
+    }
+
+    public static double soma(double n1, double n2, double n3, double n4) {
+        return n1 + n2 + n3 + n4;
+    }
+
+    public static double subtracao(double n1, double n2) {
+        return n1 - n2;
+    }
+
+    public static double subtracao(double n1, double n2, double n3) {
+        return n1 - n2 - n3;
+    }
+
+    public static double subtracao(double n1, double n2, double n3, double n4) {
+        return n1 - n2 - n3 - n4;
+    }
+
+    // multiplicação
+    public static double multiplicar(double n1, double n2) {
+        return n1 * n2;
+    }
+
+    public static double multiplicar(double n1, double n2, double n3) {
+        return n1 * n2 * n3;
+    }
+
+    public static double multiplicar(double n1, double n2, double n3, double n4) {
+        return n1 * n2 * n3 * n4;
+    }
+
+    public static double dividir(double n1, double n2) {
+        return n1 / n2;
+    }
+    public static double dividir(double n1, double n2, double n3) {
+        return n1/n2/n3;
+    }
+    public static double dividir(double n1, double n2, double n3, double n4) {
+        return n1/n2/n3/n4;
+    }
+}
